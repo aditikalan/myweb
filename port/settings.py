70bb2d4db9ea67420c2218e26c8cbf767bc353ca
Aditi Kalan <84493674+aditikalan@users.noreply.github.com>
2022-02-12 20:14:14 +0530

    Update settings.py

--- port/settings.py
+++ port/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#feo%ir_-uo348jsmkuub%4o)tb!yw#%ku&f16qu0k_&mch#l!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','aditisystem9.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','aditisystem9.herokuapp.com']
 
 
 # Application definition
